123edbe7507b7f80a4047b3bc3f9859b7d6b7adf
Andrew Matheny <androidmatheny@gmail.com>
2019-10-20 19:46:19 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,9 +1,8 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"
-        },
-      :matriarch => {
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"
         #your key/value pairs here
         },
       :hero => {
